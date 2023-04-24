acec9d35f0b469b6722a7c1a5b7bc397a300e529
xjh <934622645@qq.com>
2023-04-24 22:01:36 +0800

    hello world c version

---
+++ hello_world.c
@@ -0,0 +1,10 @@
+//
+// Created by xjh on 4/24/23.
+//
+
+#include <stdio.h>
+#include "hello_world.h"
+
+int main() {
+    printf("hello world");
+}
---
+++ hello_world.h
@@ -0,0 +1,8 @@
+//
+// Created by xjh on 4/24/23.
+//
+
+#ifndef LINUX_HELLO_WORLD_H
+#define LINUX_HELLO_WORLD_H
+
+#endif //LINUX_HELLO_WORLD_H

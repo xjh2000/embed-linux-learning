daff4a5514f1ce651ad0562c22352f38be7a2b1b
xjh <934622645@qq.com>
2023-04-24 21:30:18 +0800

    hello world

---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+//
+// Created by xjh on 4/24/23.
+//
+
+#include "HelloWorld.h"
+#include "cstdio"
+
+int main() {
+    printf("hello World");
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # embed-linux-learning
+
+learn some simple application with the embed linux , and learn linux structure
